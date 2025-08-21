656ec2195a30cfb61338299fead48d98da68733e
sandeepshahi <s.sandeepshahi@gmail.com>
2025-08-21 21:40:07 +0530

    fix: update config for common docker env

--- src/config/auth-config.ts
+++ src/config/auth-config.ts
@@ -3,7 +3,7 @@
 
 const config = {
 	JWT_SECRET: process.env.JWT_SECRET || "test-token-secret",
-	CLIENT_ID: process.env.CLIENT_ID || "test-client-123",
+	CLIENT_ID: process.env.REACT_APP_CLIENT_ID || "test-client-123",
 };
 
 export default config;
